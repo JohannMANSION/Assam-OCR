907d053feb4005a922eafe5f0fa69104df6b542a
Johann MANSION <j.mansion26@gmail.com>
2020-04-15 16:49:30 +0200

    Correction ajour images p*m

--- Visual/Assam-OCR/Assam-OCR/main.c
+++ Visual/Assam-OCR/Assam-OCR/main.c
@@ -1,18 +1,46 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <string.h>
 #include <time.h>
+
 #include "libImages.h"
 #include "libOutils.h"
 
 
+typedef char NOM[100]; // nom des chemin+images born� � 100 caract�res
+
 void main(void)
 {
-	printf("Bonjour");
-	int res;
-	res = factorielle(3);
+	FILE *F = NULL;
+	NOM *liste = NULL; // liste des images
+	NOM ligne;
+	int nb = 0;
 
+	char *nom = NULL;
+
+	// mise en m�moire images pgm/ppm 
+
+	system("dir /S /B *.p*m > listeDesImages.txt");
+
+	F = fopen("listeDesImages.txt", "r");
+	while (fscanf(F, "%s", ligne) > 0)
+	{
+		if (!liste)
+		{
+			liste = (NOM*)malloc(sizeof(NOM));
+			strcpy(liste[nb], ligne);
+			nb++;
+		}
+		else
+		{
+			liste = (NOM*)realloc(liste, sizeof(NOM)*(++nb));
+			strcpy(liste[nb - 1], ligne);
+		}
+	}
+	fclose(F);
+
+	printf("Derniere entree : %s\n", liste[nb - 1]);
 
 	system("pause");
 }
